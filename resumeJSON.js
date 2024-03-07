4aa1d5c1aa9b075edffb6af2c411aba31564aa9e
SachinJ07 <116252463+SachinJ07@users.noreply.github.com>
2024-03-07 12:37:57 +0530

    Add files via upload

---
+++ resumeJSON.js
@@ -0,0 +1,29 @@
+let resume = {
+    firstName:"Sachin",
+    lastName:"Johnson",
+    PhoneNumber:"9080095410",
+    Gmail:"jsachinjones@gmail.com",
+    Address:"2/105,Ambarampalayam,pollachi.",
+    Skills:["HTML5","C.S.S,JavaScript",],
+    education:"M.S.W",
+    languages:["Tamil","English"],
+    
+};
+console.log(resume);
+
+
+
+
+/*
+Output
+{
+  firstName: 'Sachin',
+  lastName: 'Johnson',
+  PhoneNumber: '9080095410',
+  Gmail: 'jsachinjones@gmail.com',
+  Address: '2/105,Ambarampalayam,pollachi.',
+  Skills: [ 'HTML5', 'C.S.S,JavaScript' ],
+  education: 'M.S.W',
+  languages: [ 'Tamil', 'English' ]
+}
+*/
